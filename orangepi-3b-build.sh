4e1e352395cbbc02d844a3e8a536725ca63396b6
w568w <1278297578@qq.com>
2023-12-06 21:56:07 +0800

    Revert "feat: better build script that builds miniloader binary and trust image"
    This reverts commit 84a6b2f905c0dc9103075d20c612eeadaf00293f.

--- orangepi-3b-build.sh
+++ orangepi-3b-build.sh
@@ -1,39 +1,9 @@
 #!/bin/bash
 
-rm --recursive --force out
+make mrproper
 cp --recursive rkbin /tmp 
-
-# setup your build target here!
-readonly RK_BOOT_INI=RKBOOT/RK3566MINIALL.ini # relative to ./rkbin
-readonly RK_TRUST_INI=RKTRUST/RK3568TRUST.ini # relative to ./rkbin
-readonly RK_BL31=bin/rk35/rk3568_bl31_v1.43.elf # relative to ./rkbin, need to be changed according to rkbin repo
-readonly BOARD_CONFIG_NAME=orangepi-3b-rk3566_defconfig
-
-# read TPL and BL31 from BOOT_INI
-RK_ROCKCHIP_TPL=$(grep --only-matching --perl-regexp '(?<==).*\.bin' rkbin/"$RK_BOOT_INI" | grep "ddr" | head --lines=1)
-readonly RK_ROCKCHIP_TPL
-RK_LOADER_OUTPUT=$(grep --only-matching --perl-regexp '(?<=PATH=).*' rkbin/"$RK_BOOT_INI" | head --lines=1)
-readonly RK_LOADER_OUTPUT
-RK_TRUST_OUTPUT=$(grep --only-matching --perl-regexp '(?<=PATH=).*' rkbin/"$RK_TRUST_INI" | head --lines=1)
-readonly RK_TRUST_OUTPUT
-
-# build u-boot
-export ROCKCHIP_TPL=/tmp/rkbin/"$RK_ROCKCHIP_TPL"
-export BL31=/tmp/rkbin/"$RK_BL31"
-make CROSS_COMPILE=aarch64-linux-gnu- "$BOARD_CONFIG_NAME"
+export ROCKCHIP_TPL=/tmp/rkbin/rk35/rk3566_ddr_1056MHz_v1.10.bin
+export BL31=/tmp/rkbin/rk35/rk3568_bl31_v1.32.elf
+make CROSS_COMPILE=aarch64-linux-gnu- orangepi-3b-rk3566_defconfig
 make CROSS_COMPILE=aarch64-linux-gnu- --jobs="$(nproc)" all
-unset ROCKCHIP_TPL
-unset BL31
-
-# build MiniLoaderAll.bin and trust.img
-pushd /tmp/rkbin || exit 1
-tools/boot_merger "$RK_BOOT_INI"
-tools/trust_merger "$RK_TRUST_INI"
-popd || exit 1
-
-mkdir --parents out
-mv /tmp/rkbin/"$RK_TRUST_OUTPUT" out/trust.img # need to be changed according to RKTRUST/RK3568TRUST.ini
-mv /tmp/rkbin/"$RK_LOADER_OUTPUT" out/MiniLoaderAll.bin # need to be changed according to RKBOOT/RK3566MINIALL.ini
-mv u-boot.img out/u-boot.img
-
 rm --recursive --force /tmp/rkbin
