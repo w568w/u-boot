096776cc366b61888ab5e50b773b386975652c5b
w568w <1278297578@qq.com>
2024-03-31 13:10:47 +0800

    update: TPL and BL31 version

--- orangepi-3b-build.sh
+++ orangepi-3b-build.sh
@@ -3,9 +3,9 @@
 make mrproper
 cp --recursive rkbin /tmp 
 # Rockchip's proprietary TPL to initialize the DRAM
-export ROCKCHIP_TPL=/tmp/rkbin/bin/rk35/rk3566_ddr_1056MHz_v1.18.bin
+export ROCKCHIP_TPL=/tmp/rkbin/bin/rk35/rk3566_ddr_1056MHz_v1.21.bin
 # Rockchip's proprietary TF-A to initialize the secure environment
-export BL31=/tmp/rkbin/bin/rk35/rk3568_bl31_v1.43.elf
+export BL31=/tmp/rkbin/bin/rk35/rk3568_bl31_v1.44.elf
 make CROSS_COMPILE=aarch64-linux-gnu- orangepi-3b-rk3566_defconfig
 make CROSS_COMPILE=aarch64-linux-gnu- --jobs all
 rm --recursive --force /tmp/rkbin
